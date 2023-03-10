71210ee97a59c7642903eab264078b7a5882fb26
BeRobi <screengsm@gmail.com>
2023-03-10 17:36:26 +0100

    képek módosítása -JS-

---
+++ main.js
@@ -0,0 +1,43 @@
+window.addEventListener("load", init);
+
+function init() {
+  const KEPEK = [
+    "./pic1.JPG",
+    "./pic2.JPG",
+    "./pic3.JPG",
+    "./pic4.JPG",
+    "./pic5.JPG",
+  ];
+
+  const KEPEKHELYE = document.getElementsByTagName("article");
+  let kep = kepetBeszur(KEPEK);
+
+  kiir(kep, KEPEKHELYE[0]);
+
+  const KEPSZEGELY = document.querySelectorAll("img");
+
+  for (let index = 0; index < KEPEK.length; index++) {
+    KEPSZEGELY[index].addEventListener("mouseover", kepSzegelye);
+  }
+}
+
+function kepetBeszur(KEPEK) {
+  let kephelye = "";
+  for (let index = 0; index < KEPEK.length; index++) {
+    kephelye += `<div> <img src="${KEPEK[index]}" alt="${[
+      "kep" + (index + 1),
+    ]}"> </div>`;
+  }
+  kephelye += "";
+  return kephelye;
+}
+
+function kiir(mit, hova) {
+  hova.innerHTML += mit;
+}
+
+function kepSzegelye(event) {
+  const ELEM = event.target.parentNode;
+  ELEM.classList.add("szegely");
+  console.log(event.target.parentNode);
+}
